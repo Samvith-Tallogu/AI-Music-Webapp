e0c40eb1b7acb1e3ca45583368bcc86480f22c14
Samvith-Tallogu <77870783+Samvith-Tallogu@users.noreply.github.com>
2021-08-04 13:58:40 +0530

    3rd Part

--- main.js
+++ main.js
@@ -10,8 +10,27 @@ function setup() {
     canvas.center();
     video = createCapture(VIDEO);
     video.hide();
+    poseNet = poseNet = ml5.poseNet(video, modelloaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function gotPoses(results) {
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        console.log("LeftWristX ="+ leftWristX + "LeftWristY ="+leftWristY );
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+        console.log("rightWristX ="+ rightWristX + "rightWristY ="+rightWristY );
+    }
+}
+
+function modelloaded() {
+    console.log("Model Is Loaded!");
 }
 
 function draw() {
     image(video, 0, 0, 600, 450);
 }
+
